fbd936ce22857c7c8624c637c62b34801ef49ff7
Erik Wiker <Wiker@dhcp-036072.wlan.ntnu.no>
2014-11-29 16:48:48 +0100

    Fixedyourshit.gif

--- pages/js/servers.js
+++ pages/js/servers.js
@@ -3,23 +3,23 @@ window.addEventListener('load', function() {
     var serverlist = new Array();
 
     serverlist[0] = "Choose game";
-    serverlist[1] = "masse Isaac text";
-    serverlist[2] = "serves";
-    serverlist[3] = "Fuckall";
-    serverlist[4] = "FuckLars.jpeg";
+    serverlist[1] = ["masse Isaac text", "hei1", "hei2"];
+    serverlist[2] = ["serves", "hfjdislnf"];
+    serverlist[3] = ["Fuckall"];
+    serverlist[4] = ["FuckLars.jpeg"];
 
 
 
     document.getElementById('menu').addEventListener('change', function(e) {
+
         var e = document.getElementById("games");
         var strUser = e.options[e.selectedIndex].value;
         var serverContent = document.getElementById('servers');
-        serverContent.innerHTML = serverlist[strUser];
+        serverContent.innerHTML = "";
+        for (var i = 0; i < serverlist[strUser].length; i++) {
+            serverContent.innerHTML += serverlist[strUser][i] + "</br>";
+        };
 
     });
-
-
-
-
 
 });
