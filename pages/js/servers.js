548afbdf4dda3a5925067fe731642c513e140be9
Erik Wiker <Wiker@dhcp-036072.wlan.ntnu.no>
2014-11-29 19:33:13 +0100

    Styled serverlist

--- pages/js/servers.js
+++ pages/js/servers.js
@@ -2,11 +2,11 @@ window.addEventListener('load', function() {
 
     var serverlist = new Array();
 
-    serverlist[0] = "Choose game";
-    serverlist[1] = ["masse Isaac text", "hei1", "hei2"];
-    serverlist[2] = ["serves", "hfjdislnf"];
-    serverlist[3] = ["Fuckall"];
-    serverlist[4] = ["FuckLars.jpeg"];
+    serverlist[0] = ["Choose game"];
+    serverlist[1] = ["[Санкт-Петербургский] Public [Dust2] ", "Europa Gaming | Urime 102 Vjetori Shqipni", "OLD SCHOOL CS "];
+    serverlist[2] = [" Noobs Only! Learn to Play | Fast Respawn | Rank Cap | [SkL] ", " [WBKS] Gaming#01 - Metro - NoRules {3000T/NoobMagnet/AllWeapo"];
+    serverlist[3] = ["play.montycraft.com","play.heroicmc.com","play.velocity-mc.com","OP.PrisonMine.Com"];
+    serverlist[4] = ["CRACKED SERVER by K7 ", "|MM|Playground! ", "EURO RIFLES S&D "];
 
 
 
@@ -14,7 +14,7 @@ window.addEventListener('load', function() {
 
         var e = document.getElementById("games");
         var strUser = e.options[e.selectedIndex].value;
-        var serverContent = document.getElementById('servers');
+        var serverContent = document.getElementById('servers-content');
         serverContent.innerHTML = "";
         for (var i = 0; i < serverlist[strUser].length; i++) {
             serverContent.innerHTML += serverlist[strUser][i] + "</br>";
